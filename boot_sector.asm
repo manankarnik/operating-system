868da941895588c925bb7858586056ab0c01934e
Manan Karnik <manankarnik03@gmail.com>
2022-10-12 15:22:47 +0530

    Boot sector writes string and loops

--- boot_sector.asm
+++ boot_sector.asm
@@ -1,8 +1,26 @@
-mov ah, 0x0e
-mov al, 'A'
-int 0x010
+[org 0x7c00]
+
+mov si, STR
+call printf
 
 jmp $
+
+printf:
+	pusha
+	char_loop:
+		mov al, [si]
+		cmp al, 0
+		jne print_char
+		popa
+		ret
+
+	print_char:
+		mov ah, 0x0e
+		int 0x10
+		add si, 1
+		jmp char_loop
+
+STR: db "Hello World!", 0
 
 ;; Padding and magic number
 times 510 - ($ - $$) db 0
