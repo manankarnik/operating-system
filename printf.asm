3da4d615410b2f0f4946c8898d3b7c0de3d3decf
Manan Karnik <manankarnik03@gmail.com>
2022-10-12 21:15:15 +0530

    Renamed labels

--- printf.asm
+++ printf.asm
@@ -2,13 +2,13 @@ printf:
 	push ax
 	mov ah, 0x0e				;; Move cursor forward
 
-	loop:
+	printf_loop:
 		mov al, [si]			;; Move current char pointed by si to al
 		cmp al, 0				;; Check if al is 0
-		je break				;; If equal break
+		je printf_break				;; If equal break
 		int 0x10				;; Write
 		inc si					;; Increment si
-		jmp loop
-	break:
+		jmp printf_loop
+	printf_break:
 		pop ax
 		ret
