89266243d71f0e51dc72b20d98aacd5bfa4cb71f
Manan Karnik <manankarnik03@gmail.com>
2022-10-12 18:32:35 +0530

    Replaced scripts with makefile

--- makeiso.sh
+++
@@ -1,4 +0,0 @@
-echo "Truncating..."
-truncate boot_sector.bin -s 1200k
-echo "Converting..."
-mkisofs -o os.iso -b boot_sector.bin ./
--- run.sh
+++
@@ -1,4 +0,0 @@
-echo "Building..."
-nasm boot_sector.asm -f bin -o boot_sector.bin
-echo "Running..."
-qemu-system-x86_64 boot_sector.bin
