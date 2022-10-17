6e4f3e49c9cf772f8760f566196673a6a90197be
Manan Karnik <manankarnik03@gmail.com>
2022-10-17 18:49:21 +0530

    Added local labels

--- print.asm
+++ print.asm
@@ -1,13 +1,13 @@
 print:
 	push ax
 	mov ah, 0x0e    ; Scrolling teletype BIOS routine 
-print_loop:
+.loop:
 	mov al, [si]    ; Character to be printed (Address stored in si)
 	cmp al, 0       ; Break if 0
-	je print_break
+	je .break
 	int 0x10        ; Video display functions interrupt
 	inc si
-	jmp print_loop
-print_break:
+	jmp .loop
+.break:
 	pop ax
 	ret
