3896d10c3866b334b6d9463ac481e548f86d4210
Manan Karnik <manankarnik03@gmail.com>
2022-10-17 20:20:15 +0530

    Changed comment style

--- boot_sector.asm
+++ boot_sector.asm
@@ -1,10 +1,10 @@
-[org 0x7c00]                 ;; Address where boot sector will be loaded
-mov si, STR                  ;; Store address of string in si
-call print                   ;; Call print function
-jmp $                        ;; Loop
+[org 0x7c00]                 ; Address where boot sector will be loaded
+mov si, STR                  ; Store address of string in si
+call print                   ; Call print function
+jmp $                        ; Loop
 
-STR: db "Hello world!", 0    ;; Define string
-%include "print.asm"         ;; Include print function
+STR: db "Hello world!", 0    ; Define string
+%include "print.asm"         ; Include print function
 
-times 510 - ($ - $$) db 0    ;; Padding
-dw 0xaa55                    ;; Magic number
+times 510 - ($ - $$) db 0    ; Padding
+dw 0xaa55                    ; Magic number
