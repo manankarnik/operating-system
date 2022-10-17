621e6001e7235639f3e4be0c665b7d639736b533
Manan Karnik <manankarnik03@gmail.com>
2022-10-17 16:51:45 +0530

    Function to print hex string

---
+++ print_hex.asm
@@ -0,0 +1,23 @@
+print_hex:
+    pusha
+    mov cl, 12                    ; Used for bit shift  
+    mov di, 2                     ; Used for pattern offset
+    mov si, HEX_PATTERN           ; Move pattern in si
+hex_loop:
+    mov bx, dx                    ; Move string in bx
+    shr bx, cl                    ; Shift right cl bits
+    and bx, 0x000f                ; Bit mask
+    mov bx, [bx + HEX_TABLE]      ; Move the corresponding char to bx 
+    mov [HEX_PATTERN + di], bl    ; Move bl to the pattern
+    sub cl, 4
+    inc di
+    cmp di, 6
+    je hex_break
+    jmp hex_loop
+hex_break:
+    call print
+    popa
+    ret
+    
+HEX_TABLE: db "0123456789abcdef"
+HEX_PATTERN: db "0x0000", 0x0d, 0x0a, 0
